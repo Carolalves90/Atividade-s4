e1257ff84c9a89f648736ae3ffb1708c19567e05
carolalves <prof.carolalves90@gmail.com>
2022-04-18 18:01:43 -0300

    exemplo 2 resolvido

--- exemplo2.js
+++ exemplo2.js
@@ -1,3 +1,11 @@
 // 10 em 10	
 //Imprima na tela a partir de 20 e depois de 10 em 10 até o 100, 
 //mas em vez dos numeros 60 e 90 imprima a palavra "CONTINUE".
+
+for(let numero = 20; numero <=100; numero +=10){
+    if(numero === 60 || numero === 90){
+        console.log('CONTINUE');
+        continue;
+    }
+    console.log(numero)
+}
