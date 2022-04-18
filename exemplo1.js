7e2853111e909bd227bbf2634cb3c1d624adb639
carolalves <prof.carolalves90@gmail.com>
2022-04-18 17:53:12 -0300

    exemplo 1 resolvido

--- exemplo1.js
+++ exemplo1.js
@@ -5,3 +5,28 @@
 // b) quando chegar no número 25 interrompa a instrução e pare o loop 
 // c) quando chegar no número 10 pule a instrução
 
+// a) números de 1 a 50  
+for(let numero = 1; numero <= 50; numero++){
+  console.log(numero)
+}
+
+console.log('Fim')
+
+// b) quando chegar no número 25 interrompa a instrução e pare o loop
+for(let numero = 1; numero <= 50; numero++){
+    console.log(numero)
+    if(numero === 25)
+    break;
+}
+console.log('BREAK')
+
+// c) quando chegar no número 10 pule a instrução
+let numero = 0
+while(numero < 50) {
+    numero++;
+    if(numero === 10){
+        console.log('PULE');
+        continue;
+    }
+    console.log("Continue "+ numero);
+}
