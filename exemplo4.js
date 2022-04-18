21430600f48eaec7fe3b1e946a26a9f26e20f4c5
carolalves <prof.carolalves90@gmail.com>
2022-04-18 20:40:45 -0300

    Números pares resolvida

--- exemplo4.js
+++ exemplo4.js
@@ -1,2 +1,7 @@
 // Pares	
-//Imprima na tela os números pares existentes entre 0 e 100..
+//Imprima na tela os números pares existentes entre 0 e 100.
+
+for(let n = 0; n < 100; n = n + 2){
+    console.log(`Numeros pares ${n}`)
+}
+
