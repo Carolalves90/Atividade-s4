2d15a5ef99d5c0c472e3c1cfd28ba31bfdbf6986
carolalves <prof.carolalves90@gmail.com>
2022-04-18 20:36:34 -0300

    tabuada resolvida

--- exemplo3.js
+++ exemplo3.js
@@ -1,2 +1,9 @@
 //Tabuada	
 //Faça um programa que dado um número, imprime no (console.log) a tabuada do mesmo de 1 a 10.
+
+console.log('Tabuada')
+
+let valor = 9
+for(let n = 1; n < 11; n++){
+    console.log(`${valor} x ${n} = ${valor * n}`)
+}
