53a5ad877f42693300d2dc08e941c3ef0dc9fce6
Carol Alves <82970341+Carolalves90@users.noreply.github.com>
2022-04-19 22:49:57 -0300

    Update exemplo5.js

--- exemplo5.js
+++ exemplo5.js
@@ -1,3 +1,12 @@
 //Fatorial	
 //Crie uma função que irá receber um número e retornar o fatorial dele. por exemplo: 3! = 3 * 2 * 1 // 6
 
+function resultado(valorRecebido) {
+    let fat = 1
+    for(let numero = 1; numero <= valorRecebido; numero ++){
+    if (fat = fat * numero){
+     }
+  }     return `O fatorial de ${valorRecebido} é ${fat}`
+}
+
+console.log(resultado(5))
